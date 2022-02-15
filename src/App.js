a2e5768f8beb26a52acbcfd5f907020c46760278
abdullah-alkhareji <outsiderandco@gmail.com>
2022-02-15 20:37:57 +0300

    create react app and add navbar + router

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import CategoryList from "./components/CategoryList";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<CategoryList />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <b>Foodie</b>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/categories" className="nav-link">
+                Categories
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/blank" className="nav-link">
+                Recipes
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/blank" className="nav-link">
+                Ingrediant
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
