ec61ca73e0b68b1bcc74982e3d62c8b5df568337
Ahmad <ahmad_baq@hotmail.com>
2022-02-17 11:24:40 +0300

    fix parsing error in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,28 @@ import { Link } from "react-router-dom";
 import categoryStore from "../stores/categoryStore";
 import ingredientStore from "../stores/ingredientStore";
 import AddCategoryModal from "./AddCategoryModal";
+import AddRecipeModal from "./AddRecipeModal";
 import AddIngredientModal from "./AddIngredientModal";
 import CategoryItem from "./CategoryItem";
+import RecipeItem from "./RecipeItem";
 import IngredientItem from "./IngredientItem";
 import recipeStore from "../stores/recipeStore";
-import AddCategoryModal from "./AddCategoryModal";
-import AddRecipeModal from "./AddRecipeModal";
-import CategoryItem from "./CategoryItem";
-import RecipeItem from "./RecipeItem";
-
 
 const Home = () => {
   const categoryList = categoryStore.categories
     .map((category) => <CategoryItem key={category._id} category={category} />)
     .sort(() => Math.random() - 0.5)
     .splice(0, 3);
-  const ingredientList = ingredientStore.ingredients
-    .map((ingredient) => (
-      <IngredientItem key={ingredient._id} ingredient={ingredient} />
-    ))
+
+  const ingredientList = ingredientStore.ingredients.map((ingredient) => (
+    <IngredientItem key={ingredient._id} ingredient={ingredient} />
+  ));
+
   const recipeList = recipeStore.recipes
     .map((recipe) => <RecipeItem key={recipe._id} recipe={recipe} />)
     .sort(() => Math.random() - 0.5)
     .splice(0, 3);
+
   return (
     <>
       <div>
@@ -49,6 +48,11 @@ const Home = () => {
         </div>
       </div>
       <hr className="my-4" />
+      <div>
+        <div className="d-flex justify-content-between align-items-center w-100 py-3">
+          <h1 className="m-0">Recipes</h1>
+          <AddRecipeModal />
+        </div>
         <div className="row">
           {recipeList}
           {recipeList.length > 3 && (
@@ -64,20 +68,25 @@ const Home = () => {
         </div>
       </div>
       <hr className="my-4" />
-              <div>
+      <div>
         <div className="d-flex justify-content-between align-items-center w-100 py-3">
           <h1 className="m-0">Ingredients</h1>
           <AddIngredientModal />
         </div>
         <div className="row">
           {ingredientList}
-          {ingredientList.length > 0 && (
+          {ingredientList.length > 3 && (
             <div className="col-md-3 col-sm-12 py-2">
               <Link
                 to="/ingredients"
-          <h1 className="m-0">Recipes</h1>
-          <AddRecipeModal />
+                className="card bg-light overflow-hidden shadow-sm list--item d-flex justify-content-center align-items-center h-100"
+              >
+                <span>Show All</span>
+              </Link>
+            </div>
+          )}
         </div>
+      </div>
     </>
   );
 };
