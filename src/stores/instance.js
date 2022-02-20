431bbbafb66f83cdd107997c97de8b78b6343e85
Ahmad <ahmad_baq@hotmail.com>
2022-02-20 15:37:48 +0300

    add function of fuse.js to Home

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:8000/api",
-  baseURL: "http://192.168.100.22:8000/api",
+  baseURL: "http://localhost:8000/api",
+  //baseURL: "http://192.168.100.22:8000/api",
 });
 
 export default instance;
